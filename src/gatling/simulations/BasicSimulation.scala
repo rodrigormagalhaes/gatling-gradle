195bf5496a60368976277df8cf09b86ad6ba80fa
Rodrigo Magalhães <rodrigorm.rm@gmail.com>
2019-06-21 16:40:21 -0300

    Projeto

--- src/gatling/simulations/BasicSimulation.scala
+++ src/gatling/simulations/BasicSimulation.scala
@@ -3,13 +3,18 @@ import io.gatling.core.Predef._
 
 class BasicSimulation extends Simulation {
 
-  val httpConf = http.baseUrl("https://www.mockapi.io")
+  // 1 - Common HTTP Configuration
+  val httpConf = http
+    .baseUrl("http://localhost:8080/app/")
+    .header("Accept", "application/json")
 
-  val scn = scenario("Basic Simulation")
-    .exec(http("Teste_Rodrigo")
-      .get("/"))
-    .pause(5)
 
+  // 2 - Scenario Definition
+  val scn = scenario("First Test")
+    .exec(http("Get All Games")
+    .get("videogames"))
+
+  // 3 - Load Scenario
   setUp(
     scn.inject(atOnceUsers(1))
   ).protocols(httpConf)
